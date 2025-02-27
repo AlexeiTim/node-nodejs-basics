25cc1726ef472bb40239aa181abff8a606e4381f
timashkov <timashkov060896@gmail.com>
2025-02-27 23:13:06 +0300

    feature/read-file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import path from "path";
+import fs from "fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const targetFilePath = path.resolve(
+    import.meta.dirname,
+    "./files/fileToRead.txt"
+  );
+
+  try {
+    const fileContent = await fs.readFile(targetFilePath, "utf-8");
+    console.log(fileContent);
+  } catch (e) {
+    throw new Error("FS ERROR");
+  }
 };
 
 await read();
