a8a696654c9029d01b1355776f410f66190fb0f5
timashkov <timashkov060896@gmail.com>
2025-02-27 23:22:28 +0300

    feature/parser-argv

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const result = args.reduce((acc, arg, idx) => {
+    if (idx % 2 === 0) {
+      acc += `${arg.substring(2)}`;
+    } else {
+      acc += ` is ${arg}${idx === args.length - 1 ? "" : ", "}`;
+    }
+    return acc;
+  }, "");
+  console.log(result);
 };
 
 parseArgs();
