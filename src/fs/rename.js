af2aad411ae219b76b2db7817bfd407f11614060
timashkov <timashkov060896@gmail.com>
2025-02-27 23:04:47 +0300

    feature/rename-file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,40 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+async function isFileExist(path) {
+  try {
+    await fs.access(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 const rename = async () => {
-    // Write your code here 
+  const targetFilePath = path.resolve(
+    import.meta.dirname,
+    "./files/wrongFilename.txt"
+  );
+  const newFilePath = path.resolve(
+    import.meta.dirname,
+    "./files/properFilename.md"
+  );
+
+  const targetFileExist = await isFileExist(targetFilePath);
+  if (!targetFileExist) {
+    throw new Error("File does not exist");
+  }
+
+  const newFileExist = await isFileExist(newFilePath);
+  if (newFileExist) {
+    throw new Error("File already exist");
+  }
+
+  try {
+    await fs.rename(targetFilePath, newFilePath);
+  } catch (e) {
+    throw new Error(e);
+  }
 };
 
 await rename();
