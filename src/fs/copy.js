5683f35821bbe6ccc1c8c3cca77ff3f6164c81db
timashkov <timashkov060896@gmail.com>
2025-02-27 22:36:09 +0300

    complete 2 tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,49 @@
+import { promises as fs } from "fs";
+import path from "path";
+
+async function dirExist(path) {
+  try {
+    await fs.stat(path);
+    console.log("has dir");
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+async function copyDir(src, dest) {
+  try {
+    await fs.cp(src, dest, { recursive: true });
+    return {
+      success: true,
+    };
+  } catch (e) {
+    return {
+      success: false,
+    };
+  }
+}
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const copyDirPath = path.resolve(__dirname, "./files");
+  const targetDirPath = path.resolve(__dirname, "./files_copy");
+  const FS_OPERATION_ERROR = "FS operation failed";
+
+  const isTargetDirExist = await dirExist(copyDirPath);
+  if (!isTargetDirExist) {
+    throw new Error(FS_OPERATION_ERROR);
+  }
+
+  const isCopyDirExist = await dirExist(targetDirPath);
+  if (isCopyDirExist) {
+    throw new Error(FS_OPERATION_ERROR);
+  }
+
+  const { success } = await copyDir(copyDirPath, targetDirPath);
+  if (!success) {
+    throw new Error(FS_OPERATION_ERROR);
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const writeTextToFile = "I am fresh and young";
+  const FILE_ALREADY_EXISTS_ERROR = "FS operation failed";
+  const targetFilePath = path.resolve(import.meta.dirname, "./files/fresh.txt");
+
+  try {
+    await fs.access(targetFilePath);
+    throw new Error(FILE_ALREADY_EXISTS_ERROR);
+  } catch (e) {
+    if (e.code !== "ENOENT") {
+      throw e;
+    }
+    await fs.writeFile(targetFilePath, writeTextToFile);
+  }
 };
 
 await create();
