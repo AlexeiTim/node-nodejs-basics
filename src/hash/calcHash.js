2dd7e1b3a8a39ac1d90a071b86a3517357a07d66
timashkov <timashkov060896@gmail.com>
2025-02-28 22:18:21 +0300

    feature/hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import path from "path";
+import fs from "fs";
+import crypto from "crypto";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const targetFilePath = path.resolve(
+    import.meta.dirname,
+    "./files/fileToCalculateHashFor.txt"
+  );
+  const readStream = fs.createReadStream(targetFilePath);
+  const hash = crypto.createHash("sha256");
+
+  readStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on("end", () => {
+    console.log("END", hash.digest("hex"));
+  });
 };
 
 await calculateHash();
