8856c5486bf53cae1c7c239016aa11bbbd4c7ce6
timashkov <timashkov060896@gmail.com>
2025-03-03 23:13:19 +0300

    feature/transform-stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { pipeline } from "stream/promises";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  class ReverseTransform extends Transform {
+    _transform(chunk, encoding, cb) {
+      const reversed = chunk.toString().split("").reverse().join("");
+      cb(null, reversed);
+    }
+  }
+  process.stdin.pipe(new ReverseTransform()).pipe(process.stdout);
 };
 
 await transform();
