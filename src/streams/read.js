880571624ed5938811442729a485f423d84d6548
timashkov <timashkov060896@gmail.com>
2025-03-03 22:46:54 +0300

    feature/read-stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const targetFilePath = path.resolve(
+    import.meta.dirname,
+    "./files/fileToRead.txt"
+  );
+  const readStream = fs.createReadStream(targetFilePath);
+  readStream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
 await read();
