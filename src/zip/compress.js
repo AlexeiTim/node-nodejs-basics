c72f74539b05ebe65f963adb9f99e2685b8fdfd5
timashkov <timashkov060896@gmail.com>
2025-03-03 23:28:58 +0300

    feature/compress-decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import path from "path";
+import fs from "fs";
+import { createGzip } from "zlib";
+import { pipeline } from "stream/promises";
+
 const compress = async () => {
-    // Write your code here 
+  const targetFilePath = path.resolve(
+    import.meta.dirname,
+    "./files/fileToCompress.txt"
+  );
+  const outputZipFile = "archive.gz";
+  await pipeline(
+    fs.createReadStream(targetFilePath),
+    createGzip(),
+    fs.createWriteStream(outputZipFile)
+  );
+  await fs.promises.unlink(targetFilePath);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import fs from "fs";
+import path from "path";
+import { pipeline } from "stream/promises";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const outputFilePath = path.resolve(
+    import.meta.dirname,
+    "./files/fileToCompress.txt"
+  );
+  const targetFilePath = path.resolve(import.meta.dirname, "../../archive.gz");
+  await pipeline(
+    fs.createReadStream(targetFilePath),
+    zlib.createGunzip(),
+    fs.createWriteStream(outputFilePath)
+  );
+  await fs.promises.unlink(targetFilePath);
 };
 
 await decompress();
